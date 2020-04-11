26b254f80693291c70f5ddcd5bbf07cc68d6d733
vigriban <vigriban@tut.by>
2020-04-11 16:38:36 +0300

    Add script and tests templates

---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+import prepare_speech
+
+
+class TransliterateTest(unittest.TestCase):
+
+    def test_smoke(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
